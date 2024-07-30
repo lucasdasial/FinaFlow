ac132f4fdeb0e33e033be78a32a40ef83cf67c32
Lucas Alves <as.lucasalves@gmail.com>
2024-07-30 19:11:43 -0400

    clear project

--- SalesWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SalesWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- SalesWeb/Models/SalesStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SalesWeb.Models
-{
-    public enum SalesStatus : int
-    {
-        Pending = 0,
-        Billing = 1,
-        caneled = 2
-    }
-}
